f84c6f93690b5a8887a5fc41ac7ba224b9e848bb
Shidramappa Jyoti <shidram.github@gmail.com>
2025-01-21 21:00:04 +0100

    Removed some of unused code

--- src/test/java/org/cognizant/mortgageapp/controller/MortgageControllerTest.java
+++ src/test/java/org/cognizant/mortgageapp/controller/MortgageControllerTest.java
@@ -8,8 +8,8 @@ import org.cognizant.mortgageapp.service.MortgageService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -27,7 +27,7 @@ public class MortgageControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private MortgageService mortgageService;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
